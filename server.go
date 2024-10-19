a3a17d9dedaf36d9bec32adc59441a34697ff5c1
fengjx <fengjianxin2012@gmail.com>
2024-10-20 01:53:08 +0800

    注册shutdown回调

--- server.go
+++ server.go
@@ -1,12 +1,12 @@
 package luchen
 
 import (
-	"context"
 	"errors"
 	"net/http"
 	"sync"
 	"time"
 
+	"github.com/fengjx/go-halo/halo"
 	"github.com/fengjx/go-halo/hook"
 	"go.uber.org/zap"
 
@@ -112,15 +112,10 @@ func (s *baseServer) GetServiceInfo() *ServiceInfo {
 
 // Start 启动服务
 func Start(svrs ...Server) {
-	start(nil, svrs...)
+	start(svrs...)
 }
 
-// StartWithContext 启动服务
-func StartWithContext(ctx context.Context, svrs ...Server) {
-	start(ctx, svrs...)
-}
-
-func start(ctx context.Context, svrs ...Server) {
+func start(svrs ...Server) {
 	servers = svrs
 	for _, server := range servers {
 		svr := server
@@ -134,13 +129,9 @@ func start(ctx context.Context, svrs ...Server) {
 			}
 		}()
 	}
-	if ctx == nil {
-		return
-	}
-	select {
-	case <-ctx.Done():
+	halo.AddShutdownCallback(func() {
 		Stop()
-	}
+	})
 }
 
 // Stop 停止服务
